56e710f80846ce7a4a0c5134b34af366de5a170b
Hunter Blaze <25360019+blazegeek@users.noreply.github.com>
2021-01-03 17:50:07 -0500

    add missing semicolons

--- src/types.js
+++ src/types.js
@@ -1,27 +1,27 @@
-var typeforce = require('typeforce')
+var typeforce = require('typeforce');
 
-var UINT31_MAX = Math.pow(2, 31) - 1
+var UINT31_MAX = Math.pow(2, 31) - 1;
 function UInt31 (value) {
-  return typeforce.UInt32(value) && value <= UINT31_MAX
+  return typeforce.UInt32(value) && value <= UINT31_MAX;
 }
 
 function BIP32Path (value) {
-  return typeforce.String(value) && value.match(/^(m\/)?(\d+'?\/)*\d+'?$/)
+  return typeforce.String(value) && value.match(/^(m\/)?(\d+'?\/)*\d+'?$/);
 }
-BIP32Path.toJSON = function () { return 'BIP32 derivation path' }
+BIP32Path.toJSON = function () { return 'BIP32 derivation path'; };
 
-var SATOSHI_MAX = 21 * 1e14
+var SATOSHI_MAX = 21 * 1e14;
 function Satoshi (value) {
-  return typeforce.UInt53(value) && value <= SATOSHI_MAX
+  return typeforce.UInt53(value) && value <= SATOSHI_MAX;
 }
 
 // external dependent types
-var BigInt = typeforce.quacksLike('BigInteger')
-var ECPoint = typeforce.quacksLike('Point')
+var BigInt = typeforce.quacksLike('BigInteger');
+var ECPoint = typeforce.quacksLike('Point');
 
 // exposed, external API
-var ECSignature = typeforce.compile({ r: BigInt, s: BigInt })
-var networkVersion = typeforce.oneOf(typeforce.UInt8, typeforce.UInt16)
+var ECSignature = typeforce.compile({ r: BigInt, s: BigInt });
+var networkVersion = typeforce.oneOf(typeforce.UInt8, typeforce.UInt16);
 var Network = typeforce.compile({
   messagePrefix: typeforce.oneOf(typeforce.Buffer, typeforce.String),
   bip32: {
@@ -31,7 +31,7 @@ var Network = typeforce.compile({
   pubKeyHash: networkVersion,
   scriptHash: networkVersion,
   wif: typeforce.UInt8
-})
+});
 
 // extend typeforce types with ours
 var types = {
@@ -46,10 +46,10 @@ var types = {
   Satoshi: Satoshi,
   UInt31: UInt31,
   NetworkVersion: networkVersion
-}
+};
 
 for (var typeName in typeforce) {
-  types[typeName] = typeforce[typeName]
+  types[typeName] = typeforce[typeName];
 }
 
-module.exports = types
+module.exports = types;
