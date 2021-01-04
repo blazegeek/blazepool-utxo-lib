7944d9ae3709bfbf1e7e677682055673ef4e2f43
Hunter Blaze <25360019+blazegeek@users.noreply.github.com>
2021-01-03 20:25:32 -0500

    add missing semicolons
    change ' to "

--- src/crypto.js
+++ src/crypto.js
@@ -1,31 +1,31 @@
-var createHash = require('create-hash')
-var crypto = require('crypto')
+var createHash = require("create-hash");
+var crypto = require("crypto");
 
 function ripemd160 (buffer) {
-  var hash = 'rmd160'
-  var supportedHashes = crypto.getHashes()
+  var hash = "rmd160";
+  var supportedHashes = crypto.getHashes();
   // some environments (electron) only support the long alias
-  if (supportedHashes.indexOf(hash) === -1 && supportedHashes.indexOf('ripemd160') !== -1) {
-    hash = 'ripemd160'
+  if (supportedHashes.indexOf(hash) === -1 && supportedHashes.indexOf("ripemd160") !== -1) {
+    hash = "ripemd160";
   }
 
-  return createHash(hash).update(buffer).digest()
+  return createHash(hash).update(buffer).digest();
 }
 
 function sha1 (buffer) {
-  return createHash('sha1').update(buffer).digest()
+  return createHash("sha1").update(buffer).digest();
 }
 
 function sha256 (buffer) {
-  return createHash('sha256').update(buffer).digest()
+  return createHash("sha256").update(buffer).digest();
 }
 
 function hash160 (buffer) {
-  return ripemd160(sha256(buffer))
+  return ripemd160(sha256(buffer));
 }
 
 function hash256 (buffer) {
-  return sha256(sha256(buffer))
+  return sha256(sha256(buffer));
 }
 
 module.exports = {
@@ -34,4 +34,4 @@ module.exports = {
   ripemd160: ripemd160,
   sha1: sha1,
   sha256: sha256
-}
+};
