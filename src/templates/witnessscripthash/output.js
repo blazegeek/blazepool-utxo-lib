e787d92275555eedf1fb4a36a20cb7047dcdfd49
Hunter Blaze <25360019+blazegeek@users.noreply.github.com>
2021-01-03 21:26:06 -0500

    add missing semicolons
    change ' to "

--- src/templates/witnessscripthash/output.js
+++ src/templates/witnessscripthash/output.js
@@ -1,33 +1,33 @@
 // OP_0 {scriptHash}
 
-var bscript = require('../../script')
-var types = require('../../types')
-var typeforce = require('typeforce')
-var OPS = require('bitcoin-ops')
+var bscript = require("../../script");
+var types = require("../../types");
+var typeforce = require("typeforce");
+var OPS = require("bitcoin-ops");
 
 function check (script) {
-  var buffer = bscript.compile(script)
+  var buffer = bscript.compile(script);
 
   return buffer.length === 34 &&
     buffer[0] === OPS.OP_0 &&
-    buffer[1] === 0x20
+    buffer[1] === 0x20;
 }
-check.toJSON = function () { return 'Witness scriptHash output' }
+check.toJSON = function () { return "Witness scriptHash output"; };
 
 function encode (scriptHash) {
-  typeforce(types.Hash256bit, scriptHash)
+  typeforce(types.Hash256bit, scriptHash);
 
-  return bscript.compile([OPS.OP_0, scriptHash])
+  return bscript.compile([OPS.OP_0, scriptHash]);
 }
 
 function decode (buffer) {
-  typeforce(check, buffer)
+  typeforce(check, buffer);
 
-  return buffer.slice(2)
+  return buffer.slice(2);
 }
 
 module.exports = {
   check: check,
   decode: decode,
   encode: encode
-}
+};
