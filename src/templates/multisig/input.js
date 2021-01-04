c00ae0d05099637a82e26a0f4609913189125128
Hunter Blaze <25360019+blazegeek@users.noreply.github.com>
2021-01-03 21:42:36 -0500

    Update input.js

--- src/templates/multisig/input.js
+++ src/templates/multisig/input.js
@@ -1,65 +1,65 @@
 // OP_0 [signatures ...]
 
-var Buffer = require('safe-buffer').Buffer
-var bscript = require('../../script')
-var p2mso = require('./output')
-var typeforce = require('typeforce')
-var OPS = require('bitcoin-ops')
+var Buffer = require("safe-buffer").Buffer;
+var bscript = require("../../script");
+var p2mso = require("./output");
+var typeforce = require("typeforce");
+var OPS = require("bitcoin-ops");
 
 function partialSignature (value) {
-  return value === OPS.OP_0 || bscript.isCanonicalSignature(value)
+  return value === OPS.OP_0 || bscript.isCanonicalSignature(value);
 }
 
 function check (script, allowIncomplete) {
-  var chunks = bscript.decompile(script)
-  if (chunks.length < 2) return false
-  if (chunks[0] !== OPS.OP_0) return false
+  var chunks = bscript.decompile(script);
+  if (chunks.length < 2) return false;
+  if (chunks[0] !== OPS.OP_0) return false;
 
   if (allowIncomplete) {
-    return chunks.slice(1).every(partialSignature)
+    return chunks.slice(1).every(partialSignature);
   }
 
-  return chunks.slice(1).every(bscript.isCanonicalSignature)
+  return chunks.slice(1).every(bscript.isCanonicalSignature);
 }
-check.toJSON = function () { return 'multisig input' }
+check.toJSON = function () { return "multisig input"; };
 
-var EMPTY_BUFFER = Buffer.allocUnsafe(0)
+var EMPTY_BUFFER = Buffer.allocUnsafe(0);
 
 function encodeStack (signatures, scriptPubKey) {
-  typeforce([partialSignature], signatures)
+  typeforce([partialSignature], signatures);
 
   if (scriptPubKey) {
-    var scriptData = p2mso.decode(scriptPubKey)
+    var scriptData = p2mso.decode(scriptPubKey);
 
     if (signatures.length < scriptData.m) {
-      throw new TypeError('Not enough signatures provided')
+      throw new TypeError("Not enough signatures provided");
     }
 
     if (signatures.length > scriptData.pubKeys.length) {
-      throw new TypeError('Too many signatures provided')
+      throw new TypeError("Too many signatures provided");
     }
   }
 
   return [].concat(EMPTY_BUFFER, signatures.map(function (sig) {
     if (sig === OPS.OP_0) {
-      return EMPTY_BUFFER
+      return EMPTY_BUFFER;
     }
-    return sig
-  }))
+    return sig;
+  }));
 }
 
 function encode (signatures, scriptPubKey) {
-  return bscript.compile(encodeStack(signatures, scriptPubKey))
+  return bscript.compile(encodeStack(signatures, scriptPubKey));
 }
 
 function decodeStack (stack, allowIncomplete) {
-  typeforce(check, stack, allowIncomplete)
-  return stack.slice(1)
+  typeforce(check, stack, allowIncomplete);
+  return stack.slice(1);
 }
 
 function decode (buffer, allowIncomplete) {
-  var stack = bscript.decompile(buffer)
-  return decodeStack(stack, allowIncomplete)
+  var stack = bscript.decompile(buffer);
+  return decodeStack(stack, allowIncomplete);
 }
 
 module.exports = {
@@ -68,4 +68,4 @@ module.exports = {
   decodeStack: decodeStack,
   encode: encode,
   encodeStack: encodeStack
-}
+};
