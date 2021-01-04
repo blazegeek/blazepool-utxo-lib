467ef4b565f417c07ff97befd99e3fabbd87f604
Hunter Blaze <25360019+blazegeek@users.noreply.github.com>
2021-01-03 21:35:53 -0500

    Update input.js

--- src/templates/scripthash/input.js
+++ src/templates/scripthash/input.js
@@ -1,78 +1,78 @@
 // <scriptSig> {serialized scriptPubKey script}
 
-var Buffer = require('safe-buffer').Buffer
-var bscript = require('../../script')
-var typeforce = require('typeforce')
+var Buffer = require("safe-buffer").Buffer;
+var bscript = require("../../script");
+var typeforce = require("typeforce");
 
-var p2ms = require('../multisig/')
-var p2pk = require('../pubkey/')
-var p2pkh = require('../pubkeyhash/')
-var p2wpkho = require('../witnesspubkeyhash/output')
-var p2wsho = require('../witnessscripthash/output')
+var p2ms = require("../multisig/");
+var p2pk = require("../pubkey/");
+var p2pkh = require("../pubkeyhash/");
+var p2wpkho = require("../witnesspubkeyhash/output");
+var p2wsho = require("../witnessscripthash/output");
 
 function check (script, allowIncomplete) {
-  var chunks = bscript.decompile(script)
-  if (chunks.length < 1) return false
+  var chunks = bscript.decompile(script);
+  if (chunks.length < 1) return false;
 
-  var lastChunk = chunks[chunks.length - 1]
-  if (!Buffer.isBuffer(lastChunk)) return false
+  var lastChunk = chunks[chunks.length - 1];
+  if (!Buffer.isBuffer(lastChunk)) return false;
 
-  var scriptSigChunks = bscript.decompile(bscript.compile(chunks.slice(0, -1)))
-  var redeemScriptChunks = bscript.decompile(lastChunk)
+  var scriptSigChunks = bscript.decompile(bscript.compile(chunks.slice(0, -1)));
+  var redeemScriptChunks = bscript.decompile(lastChunk);
 
   // is redeemScript a valid script?
-  if (redeemScriptChunks.length === 0) return false
+  if (redeemScriptChunks.length === 0) return false;
 
   // is redeemScriptSig push only?
-  if (!bscript.isPushOnly(scriptSigChunks)) return false
+  if (!bscript.isPushOnly(scriptSigChunks)) return false;
 
   // is witness?
   if (chunks.length === 1) {
     return p2wsho.check(redeemScriptChunks) ||
-      p2wpkho.check(redeemScriptChunks)
+      p2wpkho.check(redeemScriptChunks);
   }
 
   // match types
   if (p2pkh.input.check(scriptSigChunks) &&
-    p2pkh.output.check(redeemScriptChunks)) return true
+    p2pkh.output.check(redeemScriptChunks)) return true;
 
   if (p2ms.input.check(scriptSigChunks, allowIncomplete) &&
-    p2ms.output.check(redeemScriptChunks)) return true
+    p2ms.output.check(redeemScriptChunks)) return true;
 
   if (p2pk.input.check(scriptSigChunks) &&
-    p2pk.output.check(redeemScriptChunks)) return true
+    p2pk.output.check(redeemScriptChunks)) return true;
 
-  return false
+  return false;
 }
-check.toJSON = function () { return 'scriptHash input' }
+check.toJSON = function () { return "scriptHash input"; };
 
 function encodeStack (redeemScriptStack, redeemScript) {
-  var serializedScriptPubKey = bscript.compile(redeemScript)
+  var serializedScriptPubKey = bscript.compile(redeemScript);
 
-  return [].concat(redeemScriptStack, serializedScriptPubKey)
+  return [].concat(redeemScriptStack, serializedScriptPubKey);
 }
 
 function encode (redeemScriptSig, redeemScript) {
-  var redeemScriptStack = bscript.decompile(redeemScriptSig)
+  var redeemScriptStack = bscript.decompile(redeemScriptSig);
 
-  return bscript.compile(encodeStack(redeemScriptStack, redeemScript))
+  return bscript.compile(encodeStack(redeemScriptStack, redeemScript));
 }
 
 function decodeStack (stack) {
-  typeforce(check, stack)
+  typeforce(check, stack);
 
   return {
     redeemScriptStack: stack.slice(0, -1),
     redeemScript: stack[stack.length - 1]
-  }
+  };
 }
 
 function decode (buffer) {
-  var stack = bscript.decompile(buffer)
-  var result = decodeStack(stack)
-  result.redeemScriptSig = bscript.compile(result.redeemScriptStack)
-  delete result.redeemScriptStack
-  return result
+  var stack = bscript.decompile(buffer);
+  var result = decodeStack(stack);
+  result.redeemScriptSig = bscript.compile(result.redeemScriptStack);
+  delete result.redeemScriptStack;
+  return result;
 }
 
 module.exports = {
@@ -81,4 +81,4 @@ module.exports = {
   decodeStack: decodeStack,
   encode: encode,
   encodeStack: encodeStack
-}
+};
