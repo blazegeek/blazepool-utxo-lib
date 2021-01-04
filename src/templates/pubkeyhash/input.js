60ee3169212e053c9730a3c2f024fa09578c62e5
Hunter Blaze <25360019+blazegeek@users.noreply.github.com>
2021-01-03 21:38:56 -0500

    Update input.js

--- src/templates/pubkeyhash/input.js
+++ src/templates/pubkeyhash/input.js
@@ -1,16 +1,16 @@
 // {signature} {pubKey}
 
-var bscript = require('../../script')
-var typeforce = require('typeforce')
+var bscript = require("../../script");
+var typeforce = require("typeforce");
 
 function check (script) {
-  var chunks = bscript.decompile(script)
+  var chunks = bscript.decompile(script);
 
   return chunks.length === 2 &&
     bscript.isCanonicalSignature(chunks[0]) &&
-    bscript.isCanonicalPubKey(chunks[1])
+    bscript.isCanonicalPubKey(chunks[1]);
 }
-check.toJSON = function () { return 'pubKeyHash input' }
+check.toJSON = function () { return "pubKeyHash input"; };
 
 function encodeStack (signature, pubKey) {
   typeforce({
@@ -19,27 +19,27 @@ function encodeStack (signature, pubKey) {
   }, {
     signature: signature,
     pubKey: pubKey
-  })
+  });
 
-  return [signature, pubKey]
+  return [signature, pubKey];
 }
 
 function encode (signature, pubKey) {
-  return bscript.compile(encodeStack(signature, pubKey))
+  return bscript.compile(encodeStack(signature, pubKey));
 }
 
 function decodeStack (stack) {
-  typeforce(check, stack)
+  typeforce(check, stack);
 
   return {
     signature: stack[0],
     pubKey: stack[1]
-  }
+  };
 }
 
 function decode (buffer) {
-  var stack = bscript.decompile(buffer)
-  return decodeStack(stack)
+  var stack = bscript.decompile(buffer);
+  return decodeStack(stack);
 }
 
 module.exports = {
@@ -48,4 +48,4 @@ module.exports = {
   decodeStack: decodeStack,
   encode: encode,
   encodeStack: encodeStack
-}
+};
